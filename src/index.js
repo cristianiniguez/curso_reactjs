3722e0cb2d921d838b377a084e3dc8fc301e4eb4
cristianiniguez <felizpc1@gmail.com>
2020-09-27 17:39:18 -0400

    Nuestra primera página

--- src/index.js
+++ src/index.js
@@ -4,17 +4,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Cristian"
-    lastName="Iñiguez"
-    avatarUrl="https://www.gravatar.com/avatar/af9c8952c70a90472c70fb1860cea678?d=identicon"
-    jobTitle="Industrial Engineer"
-    twitter="criniguez"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
