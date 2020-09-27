413dae368753d378445ddf9f6e0613082343db85
cristianiniguez <felizpc1@gmail.com>
2020-09-27 14:44:44 -0400

    ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// const element = document.createElement("h1");
+// element.innerText = "Hello, Platzi Badges!";
 
-const container = document.getElementById('app');
+// const container = document.getElementById("app");
 
-const element = <h1>Hello, Platzi Badges</h1>;
+// container.appendChild(element);
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello, Platzi Badges from React!</h1>;
+
+const container = document.getElementById("app");
+
+// ReactDOM.render(__que__, __donde__)
 ReactDOM.render(element, container);
